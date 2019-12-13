73f44857c668a650e60efde180be2dfe6c7a46fd
lorderonnie <rontheking45@gmail.com>
2019-12-13 15:01:09 +0300

    created a base.html

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+
+
 
 # Create your views here.
+def gallery(request):
+    return HttpResponse(request,'home.html')
+
+
+
+   
+
+
+
+
+
+
