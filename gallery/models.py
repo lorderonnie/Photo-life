6f1eed784bff433e1426dacfbc3eea25be5856a4
lorderonnie <rontheking45@gmail.com>
2019-12-13 17:03:42 +0300

    added more model classes

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,28 @@ class Pictionary(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     
+    def __str__(self):
+        return self.first_name
+    def save_pictionary(self):
+        self.save()
+
+class Category(models.Model):
+    category_name = models.CharField(max_length =70)
+    def __str__(self):
+        return self.category_name
+
+class Location(models.Model):
+    location_name = models.CharField(max_length =70)
+    def __str__(self):
+        return self.location_name
+    
+class Photos(models.Model):
+    image = 
+    name =
+    description = 
+    location =
+    category =
+    photographer =
     
     
     
