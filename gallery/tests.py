1fab1ae7baa218f7d15efba8ec25e31b9e96e573
lorderonnie <rontheking45@gmail.com>
2019-12-18 08:37:19 +0300

    created tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Photos,Category,Location,Pictionary
 
-# Create your tests here.
+
+#creating a method
+
+class Pictionary(TestCase):
+    def setUp(self):
+        self.ronnie= Pictionary(first_name = 'ronnie',last_name='Mwambia')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ronnie,Pictionary))
+
+#creating picture function
+
+class Photos(TestCase):
+    #creating a information bar(Pictionary)and saving it
+    def setUp(self):
+        self.ronnie= Pictionary(first_name = 'ronnie',last_name='Mwambia')
+        self.ronnie.save_pictionary
+
+    #creating a new category and saving it
+        self.new_category = Category (name= 'testing')
+        self.new_category.save_category()
+    
+    
+    
+    
+    
+    
+    
